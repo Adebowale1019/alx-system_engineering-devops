6edf9eb01bef159383958e9dc5bb6f15011b5731
Adebowale1019 <adesokanadebowhale@gmail.com>
2024-03-31 01:41:49 -0700

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/*Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response*/
 
 # Check if URL argument is provided
 if [ $# -eq 0 ]; then
